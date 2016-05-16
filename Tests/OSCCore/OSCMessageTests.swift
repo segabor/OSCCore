451be6153163586d8b39961e5754fa9f68aff972
Gábor Sebestyén <segabor@gmail.com>
2016-05-16 10:15:36 +0200

    Improve message tests

--- Tests/OSCCore/OSCMessageTests.swift
+++ Tests/OSCCore/OSCMessageTests.swift
@@ -1,6 +1,8 @@
 @testable import OSCCore
 import XCTest
 
+typealias Message = (address: String, args: [OSCValue])
+
 class OSCMessageTests : XCTestCase {
     func testEmptyMessage() {
         let msg = OSCMessage(address: "hello")
@@ -12,15 +14,32 @@ class OSCMessageTests : XCTestCase {
         XCTAssertEqual(packet, value)
         
         guard
-            let parsed = msg.parse(),    
-            let parsed2 = OSCMessage(data: packet).parse()
+            let parsed : Message = msg.parse(),
+            let parsed2 : Message = OSCMessage(data: packet).parse()
         else {
             XCTFail("Failed to restore message from packet")
             return
         }
 
-        XCTAssertEqual(parsed.address, parsed2.address)
-        XCTAssertEqual(0, parsed.args.count)
-        XCTAssertEqual(0, parsed2.args.count)
+        assertOSCMessagesEqual(parsed, parsed2)
+    }
+
+    // helper method to compare OSC messages
+    private func assertOSCMessagesEqual(_ msg: Message, _ otherMsg: Message) {
+        XCTAssertEqual(msg.address, otherMsg.address)
+        assertOSCValuesEqual(msg.args, otherMsg.args)
+    }
+    
+    // helper method to compare two OSCValue arrays
+    private func assertOSCValuesEqual(_ obj: [OSCValue], _ other: [OSCValue]) {
+        XCTAssertEqual(obj.count, other.count)
+        
+        for ix in (0..<obj.count) {
+            let obj1 = obj[ix]
+            let obj2 = other[ix]
+            
+            XCTAssertEqual(obj1.oscType, obj2.oscType)
+            XCTAssertEqual(obj1.oscValue, obj2.oscValue)
+        }
     }
 }
