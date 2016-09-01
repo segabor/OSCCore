1d29eaa73e36c09bb918928b763e35425a5b90ad
Gábor Sebestyén <segabor@gmail.com>
2016-09-01 17:29:54 -0400

    OSCServer example: code cleanup

--- Examples/OSCServer/Sources/main.swift
+++ Examples/OSCServer/Sources/main.swift
@@ -3,31 +3,61 @@ import OSCCore
 import C7
 
 let clientPort    = 5050
-let pktSize       = 1024
 
-if let udpSocket = try? UDPSocket(ip: IP(port: clientPort)) {
 
-  var msg = Data() 
-  while true {
-    if let (pkt, ip) = try? udpSocket.receive(pktSize) {
-      msg += pkt
-      print("Received \(pkt.count) bytes")
 
-      if pkt.count < pktSize {
-        break
-      }
-    } else {
-      print("EOL")
-      break
-    }
-  }
-  print("Message arrived")
+func receiveOSCMessage(port : Int) -> OSCMessage? {
 
-  let osc_msg = OSCMessage(data: msg.bytes)
-  if let x = osc_msg.parse() {
-    print("address: \(x.address)")
-    x.args.forEach{ arg in print("arg: \(arg)") }
-  } else {
-    print("Failed to parse message")
-  }
+	// open socket and listen for incoming packet
+	guard let udpSocket = try? UDPSocket(ip: IP(port: clientPort))
+	else {
+		return nil
+	}
+
+
+	func catchPackets(socket : UDPSocket) -> Data? {
+		/// this buffer collects incoming packets
+		var buf = Data()
+		let maxBytes = 1024
+
+		/// run until we have the entire stuff over the channel
+		while true {
+			guard let (pkt, _) = try? socket.receive(maxBytes)
+			else {
+				/// something went wrong ...
+				return nil
+			}
+
+			/// append packet to buffer
+			buf += pkt
+
+			/// packet is less than the maximum size
+			///   this was the last packet, bye
+			if pkt.count < maxBytes {
+				return buf
+			}
+		}
+ 	}
+
+	/// fetch message over the UDP tunnel
+	guard let buf = catchPackets(socket: udpSocket)
+	else {
+		return nil
+	}
+
+	/// create OSC message
+	return OSCMessage(data: buf.bytes)
 }
+
+
+
+if let msg = receiveOSCMessage(port: clientPort),
+	let parsed_msg = msg.parse() {
+
+	/// print out message content
+	print("address: \(parsed_msg.address)")
+	parsed_msg.args.forEach { arg in
+		print("arg: \(arg)")
+	}
+}
+
