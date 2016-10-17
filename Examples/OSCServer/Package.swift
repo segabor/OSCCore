34412aa5a30d39479e2a716623585e98a571771e
Gábor Sebestyén <segabor@gmail.com>
2016-10-17 22:37:57 +0200

    Update server example

--- Examples/OSCServer/Package.swift
+++ Examples/OSCServer/Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
     name: "OSCServer",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/UDP.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/antitypical/Result", "3.0.0-alpha.4"),
         .Package(url: "../..", majorVersion: 0)
     ]
 )
--- Examples/OSCServer/Sources/main.swift
+++ Examples/OSCServer/Sources/main.swift
@@ -1,80 +1,17 @@
-import C7 /// required for Data type
-import Result
-import UDP
 import OSCCore
 
 
-// the error part of Result
-enum MyError : Error {
-	case SocketError
-	case ReceiveError
-	case ParseError
+if let srv = try? OSCServer(port: 5050) {
+  srv.register(pattern: "/hello") { msg in
+    
+    if let parsed = msg.parse() {
+      
+      print("Address: \(parsed.address)")
+      parsed.args.forEach { arg in
+        print("arg: \(arg)")
+      }
+    }
+  }
+
+  srv.listen()
 }
-
-
-// open port
-func createSocket(port : Int) -> Result<UDPSocket, MyError> {
-	if let udpSocket = try? UDPSocket(ip: IP(port: port)) {
-		return .success(udpSocket)
-	} else {
-		return .failure(.SocketError)
-	}
-}
-
-// receive message over UDP channel
-func receivePacket(_ socket : UDPSocket) -> Result<Data, MyError> {
-	if let (buf, _) = try? socket.receive(1536) {
-		print("Received \(buf.count) bytes")
-		return .success(buf)
-	} else {
-		return .failure(.ReceiveError)
-	}
-}
-
-// parse OSC message
-func parseMessage(_ buf : Data ) -> Result<ParsedMessage, MyError> {
-	let msg = OSCMessage(data: buf.bytes )
-	if let parsed = msg.parse()	{
-		return .success(parsed)
-	} else {
-		return .failure(.ParseError)
-	}
-}
-
-// register an observer
-OSCMessageDispatcher.register("/hello") { (msg: OSCMessage)->Void in 
-	print("Message arrived")	
-}
-
-// The main loop
-let result : Result<UDPSocket, MyError> = createSoclet(port: 5050)
-switch result {
-case let .success(sock):
-	while true {
-
-	}
-default:
-}
-
-// -- old -- //
-
-// let it flow ...
-let result =
-	createSocket(port: 5050)
- 	>>- receivePacket
-	>>- parseMessage
-
-
-
-// see the result!
-switch result {
-case let .success(msg):
-	/// print out message content
-	print("address: \(msg.address)")
-	msg.args.forEach { arg in
-		print("arg: \(arg)")
-	}
-case .failure(_):
-	print("Failed to retrieve message")
-}
-
