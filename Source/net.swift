6455e6712a9404ee6b1466125adc0a46e68693ff
Gábor Sebestyén <segabor@gmail.com>
2016-10-26 18:21:02 +0200

    Move networking code into one file

--- Source/net.swift
+++ Source/net.swift
@@ -1,6 +1,32 @@
 import UDP
 
 
+public class UDPClient {
+
+  public let socket: UDPSendingSocket
+
+  public init(socket: UDPSendingSocket) {
+    self.socket = socket
+  }
+
+  public convenience init?( port: Int, remotePort serverPort: Int) {
+    guard
+      let clientIP = try? IP(port: port),
+      let serverIP = try? IP(port: serverPort),
+      let sock = try? UDPSocket(ip: clientIP).sending(to: serverIP)
+    else {
+      return nil
+    }
+
+    self.init(socket: sock)
+  }
+
+  public func send(message: OSCConvertible) throws {
+    try socket.write(message.oscValue, deadline: 1.second.fromNow())
+  }
+}
+
+
 public class UDPServer: MessageDispatcher {
   public typealias Message = OSCMessage
 
@@ -41,8 +67,8 @@ public class UDPServer: MessageDispatcher {
           var msgs = [OSCMessage]()
 
           // collect messages
-          recursive{ f, bundle in
-            bundle.content.forEach{ item in
+          recursive { f, bundle in
+            bundle.content.forEach { item in
               switch item {
               case let m as OSCMessage:
                 msgs.append(m)
