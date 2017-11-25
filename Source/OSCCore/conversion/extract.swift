011fe0858cbe870f3b22f2f81e4d3f7b8f891b4a
Gábor Sebestyén <segabor@gmail.com>
2017-11-25 13:10:29 +0100

    Bugfix: bundle comes first in packet detection order

--- Source/OSCCore/conversion/extract.swift
+++ Source/OSCCore/conversion/extract.swift
@@ -5,16 +5,17 @@
 //  Created by Sebestyén Gábor on 2017. 11. 19..
 //
 
+// FIXME: cover this with tests!
 public func extract(contentOf buffer: UnsafeMutablePointer<CChar>, length: Int) -> OSCConvertible? {
     let rawBytes: [Byte] = buffer.withMemoryRebound(to: Byte.self, capacity: length) { (bytesPointer: UnsafeMutablePointer<Byte>) -> [Byte] in
         return [Byte](UnsafeBufferPointer<Byte>.init(start: bytesPointer, count: length))
     }
 
     let decodedPacket: OSCConvertible? = { rawData in
-        if let msg = OSCMessage(data: rawData ) {
-            return msg
-        } else if let bndl = OSCBundle(data: rawData) {
+        if let bndl = OSCBundle(data: rawData ) {
             return bndl
+        } else if let msg = OSCMessage(data: rawData) {
+            return msg
         }
         return nil
     }(rawBytes)
