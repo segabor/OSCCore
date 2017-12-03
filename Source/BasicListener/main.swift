39760f2e5ee8c5cb3ef83f73591c2b767f6c4d7f
Gábor Sebestyén <segabor@gmail.com>
2017-12-03 20:19:18 +0100

    Improve basic listener example

--- Source/BasicListener/main.swift
+++ Source/BasicListener/main.swift
@@ -1,10 +1,37 @@
+import Foundation
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+
 import OSCCore
 
-let listener = UDPListener(listenerPort: Int(57110))
+var port : Int = 57110
 
-listener.listen { receivedMessage in
+if CommandLine.arguments.count > 1 {
+    if let parsedPort = Int(CommandLine.arguments[CommandLine.arguments.endIndex-1]) {
+        port = parsedPort
+    } else {
+        print("Usage: \(CommandLine.arguments[0]) <port number>")
+        exit(0)
+    }
+}
+
+print("Start listening on port \(port)")
+
+let receiver = UDPListener(listenerPort: port)
+
+receiver.listen { receivedMessage in
     if let msg = receivedMessage as? OSCMessage {
-        print("Received \(msg)")
+        print("   \(msg.address): \(msg.args)")
+    } else if let bndl = receivedMessage as? OSCBundle {
+        print("\(bndl.timetag.toDate())")
+        for oscItem in bndl.content {
+            if let msg = oscItem as? OSCMessage {
+                print("    \(msg.address): \(msg.args)")
+            }
+        }
     }
     return nil
 }
